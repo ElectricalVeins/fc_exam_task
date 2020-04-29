bd23fefb899a26d643e3a20113cbab7662504066
eveins <maxmost512@gmail.com>
2020-04-30 01:12:40 +0300

    create function for creation of the log object

--- server/src/server/utils/logger.js
+++ server/src/server/utils/logger.js
@@ -5,3 +5,18 @@ const { promisify } = require( 'util' );
 const { LOG_PROPS, LOG_FILE_PATH } = require( '../../constants' )
 const stat = promisify( fs.stat );
 const open = promisify( fs.open );
+
+
+
+
+async function createLogObject( object, start, props ) {
+  const time = { time: Date.parse( new Date() ) };
+  const properties = _.pick( object, props )
+  const logInfo = _.assign( time, properties )
+  const jsonStr = JSON.stringify( logInfo )
+
+  if( start !== 1 ) {
+    return ',' + jsonStr + ']'
+  }
+  return jsonStr + ']'
+}
