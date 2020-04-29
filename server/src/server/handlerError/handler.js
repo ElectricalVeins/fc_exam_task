33722b96d859b7a055af453d9de8f724cfd39802
eveins <maxmost512@gmail.com>
2020-04-30 01:16:35 +0300

    add logToFile function call to error handler

--- server/src/server/handlerError/handler.js
+++ server/src/server/handlerError/handler.js
@@ -1,5 +1,9 @@
+const logToFile = require('../utils/logger.js')
+
 module.exports = (err, req, res, next) => {
     console.log(err);
+    logToFile(err)
+
     if (err.message === 'new row for relation "Banks" violates check constraint "Banks_balance_ck"' || err.message === 'new row for relation "Users" violates check constraint "Users_balance_ck"') {
         err.message = 'Not Enough money';
         err.code = 406;
