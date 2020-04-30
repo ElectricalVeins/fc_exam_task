7aa4efca172c3fe1751f5e67632640aa2626bc9c
eveins <maxmost512@gmail.com>
2020-04-30 22:26:32 +0300

    refactor logging functions
    delete magic number
    add try-catch constructions
    add startChar of file check

--- server/src/server/utils/logger.js
+++ server/src/server/utils/logger.js
@@ -2,59 +2,75 @@ const fs = require( 'fs' );
 const path = require( 'path' );
 const _ = require( 'lodash' );
 const { promisify } = require( 'util' );
-const { LOG_PROPS, LOG_FILE_PATH } = require( '../../constants' )
+const { LOG_PROPS, LOG_FILE_PATH, LAST_CHARS_TO_DELETE } = require( '../../constants' )
 const stat = promisify( fs.stat );
 const open = promisify( fs.open );
 
 async function checkFileExistence( fPath ) {
   try {
-    const result = await open( fPath, 'wx' )
-    console.log( 'RESULT=>', result )
-    if( result && result.code === 'EEXIST' ) {
-      return;
+    const result = await open( fPath, 'r' )
+    return typeof result === 'number';
+  } catch ( err ) {
+    if( err && ( err.code === 'EEXIST' || err.code === 'ENOENT' ) ) {
+      return false;
     }
-  } catch ( e ) {
-    console.log( 'CATCHED ERROR---', e )
-    await appendToFile( '[]', 0, LOG_FILE_PATH ) //сделать func createFile
+    throw err
   }
 }
 
 async function getFileSize( fPath ) {
-  const stats = await stat( fPath );
-  return stats.size
+  try {
+    const stats = await stat( fPath );
+    return stats.size
+  } catch ( e ) {
+    throw e
+  }
 }
-async function createLogObject( object, start, props ) {
+
+function createLogObject( object, start, props ) {
   const time = { time: Date.parse( new Date() ) };
   const properties = _.pick( object, props )
   const logInfo = _.assign( time, properties )
   const jsonStr = JSON.stringify( logInfo )
-
-  if( start !== 1 ) {
-    return ',' + jsonStr + ']'
+  if( start === 1 ) {
+    return jsonStr + ']'
   }
-  return jsonStr + ']'
+  return ',' + jsonStr + ']' //изменено
 }
 
-async function appendToFile( data, start, fPath ) {
-  /*  if( start < 0 ) {
-   return;
-   }*/
-  const streamLog = await fs.createWriteStream( fPath, {
-    flags: 'r+',
-    start
-  } )
-  await streamLog.write( data, 'utf-8' );
-  streamLog.end();
+async function appendToFile( fPath, data, start, flags ) {
+  try {
+    const streamLog = await fs.createWriteStream( fPath, {
+      flags,
+      start
+    } )
+    await streamLog.write( data, 'utf-8' );
+    streamLog.end();
+  } catch ( e ) {
+    throw e
+  }
 }
 
 async function logToFile( data ) {
   try {
-    //await checkFileExistence( LOG_FILE_PATH )
-    const start = await getFileSize( LOG_FILE_PATH ) - 1
-    console.log( 'Start Is ==>', start )
+    const isFileExist = await checkFileExistence( LOG_FILE_PATH )
+
+    if( !isFileExist ) {
+      await appendToFile( LOG_FILE_PATH, '[]', 0, 'w' );
+    }
+
+    let start = await getFileSize( LOG_FILE_PATH ) - LAST_CHARS_TO_DELETE
+
+    if( start === -1 ) {
+      start = await getFileSize( LOG_FILE_PATH ) - LAST_CHARS_TO_DELETE
+    }
+
     const appendData = await createLogObject( data, start, LOG_PROPS )
-    await appendToFile( appendData, start, LOG_FILE_PATH )
+    await appendToFile( LOG_FILE_PATH, appendData, start, 'r+' )
+
+    console.log( 'RETURN' )
   } catch ( err ) {
+    console.log( 'RETURN MAIN FUNC ERROR' )
     throw err
   }
 }
