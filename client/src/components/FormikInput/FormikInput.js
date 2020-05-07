0721dc4e9a0131202c7c7fe3498b9e3756a6adb0
eveins <maxmost512@gmail.com>
2020-05-07 22:28:17 +0300

    refactor: Input components
    remove FormikDateInput
    change FormikInput

--- client/src/components/FormikInput/FormikInput.js
+++ client/src/components/FormikInput/FormikInput.js
@@ -1,25 +1,18 @@
-import React      from 'react';
-import PropTypes  from 'prop-types';
-import styles     from './FormikInput.sass'
-import classNames from 'classnames'
+import React  from 'react';
+import styles from './FormikInput.module.sass'
 
 const FormikInput = ( { field, form, meta, ...rest } ) => {
 
-  const inputClassNames = classNames( styles.defaultClass, {
-    [ styles.invalidClass ]: meta.touched && meta.error,
-    [ styles.validClass ]: meta.touched && !meta.error,
-  } );
-
   return (
     <label className={styles.container}>
-      <input className={inputClassNames} {...field} {...rest} />
+      {
+        rest.children
+      }
       {
         meta.error && <div className={styles.errorTip}>{meta.error}</div>
       }
     </label>
   );
 };
-
-FormikInput.propTypes = {};
 
 export default FormikInput;
