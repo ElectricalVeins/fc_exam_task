f2c4f63a40691e43ecef15057001d47f4bdacebb
eveins <maxmost512@gmail.com>
2020-05-07 11:04:34 +0300

    create initial setup for events page

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import ContestCreationPage from './pages/ContestCreation/ContestCreationPage';
 import CONSTANTS from './constants';
 import browserHistory from './browserHistory';
 import ChatContainer from './components/Chat/ChatComponents/ChatContainer/ChatContainer';
-
+import Events from './pages/Events/Events'
 
 
 class App extends Component {
@@ -60,6 +60,7 @@ class App extends Component {
                     <Route exact path='/dashboard' component={PrivateHoc(Dashboard)}/>
                     <Route exact path='/contest/:id' component={PrivateHoc(ContestPage)}/>
                     <Route exact path='/account' component={PrivateHoc(UserProfile)}/>
+                    <Route exact path='/events' component={PrivateHoc(Events)}/>
                     <Route component={NotFound}/>
                 </Switch>
                 <ChatContainer/>
---
+++ client/src/pages/Events/Events.js
@@ -0,0 +1,16 @@
+import React     from 'react';
+import PropTypes from 'prop-types';
+
+const Events = props => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+Events.propTypes = {
+
+};
+
+export default Events;
