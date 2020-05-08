7926abea9152b6e2ce4cd29d772aa6c2147778f5
eveins <maxmost512@gmail.com>
2020-05-08 12:18:08 +0300

    create initial Restore Password Page setup

---
+++ client/src/pages/RestorePassword/RestorePassword.js
@@ -0,0 +1,16 @@
+import React     from 'react';
+import PropTypes from 'prop-types';
+
+const RestorePassword = props => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+RestorePassword.propTypes = {
+  
+};
+
+export default RestorePassword;
