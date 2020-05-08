55f3cb9e8a099e99df6d034481347233f8d1c3b2
eveins <maxmost512@gmail.com>
2020-05-08 11:52:39 +0300

    create ButtonGroup component

---
+++ client/src/components/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import propTypes           from 'prop-types';
+import Button              from "../Button/Button";
+import styles              from './ButtonGroup.module.sass'
+
+const ButtonGroup = props => {
+
+  const [ inputValue, setInputValue ] = useState( '' )
+
+  const clickHandler = ( value ) => {
+    setInputValue( value )
+  }
+
+  return (
+    <div className={styles.groupContainer}>
+      <input name='url_need' type="hidden" value={inputValue} readOnly/>
+      <Button clickHandler={clickHandler}
+              value='as name'
+              inputValue={inputValue}
+              description='The Domain should exactly match the name'/>
+      <Button clickHandler={clickHandler}
+              value='yes'
+              inputValue={inputValue}
+              description='But minor variations are allowed (Recommended)'/>
+      <Button clickHandler={clickHandler}
+              value='no'
+              inputValue={inputValue}
+              description='I am only looking for a name, not a Domain'/>
+
+    </div>
+  );
+};
+
+ButtonGroup.defaultProps = {};
+
+export default ButtonGroup;
