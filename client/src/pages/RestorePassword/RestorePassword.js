941d38fa1f5d62a5192f82daf07077a6dc9b1ba5
eveins <88440918DjdfCfif>
2020-05-09 12:52:13 +0300

    add restore password page markup and styles

--- client/src/pages/RestorePassword/RestorePassword.js
+++ client/src/pages/RestorePassword/RestorePassword.js
@@ -1,16 +1,29 @@
-import React     from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+import styles from './RestorePassword.module.sass'
+import CONSTANTS from "../../constants";
+import RestorePasswordForm from "../../components/RestorePasswordForm/RestorePasswordForm";
 
 const RestorePassword = props => {
-  return (
-    <div>
-
-    </div>
-  );
+    return (
+        <div className={styles.pageContainer}>
+            <div className={styles.headerRestorePage}>
+                <Link to='/'>
+                    <img src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo"/>
+                </Link>
+                <Link to='/' className={styles.linkButton}>
+                    <span>Back to Main Page</span>
+                </Link>
+            </div>
+            <div className={styles.restoreFormContainer}>
+                <h1>Password Restore Form</h1>
+                <RestorePasswordForm/>
+            </div>
+        </div>
+    );
 };
 
-RestorePassword.propTypes = {
-  
-};
+RestorePassword.propTypes = {};
 
 export default RestorePassword;
