1d43c7dc2e62ad846b9c533eef69dd77da85a61b
eveins <maxmost512@gmail.com>
2020-05-12 17:02:36 +0300

    add error code to constructor of TokenError

--- server/src/server/errors/TokenError.js
+++ server/src/server/errors/TokenError.js
@@ -1,8 +1,8 @@
 const ApplicationError = require('./ApplicationError');
 
 class TokenError extends ApplicationError {
-    constructor(message) {
-        super(message || 'token error', 408);
+    constructor(message, code) {
+        super(message || 'token error', code || 408);
     }
 }
 
