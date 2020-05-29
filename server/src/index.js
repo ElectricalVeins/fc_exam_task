e0e352274306c2f7c08975881baaa0bdb8df49bb
eveins <maxmost512@gmail.com>
2020-05-29 16:28:27 +0300

    reformat code
    eslint fix

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
 const express=require('express');
-import "babel-polyfill";
+import 'babel-polyfill';
 const router=require('./server/router');
 const cors=require('cors');
 const controller = require('./socketInit');
 const handlerError=require('./server/handlerError/handler');
 const CONSTANTS =require('./constants');
-const createLogHistory = require( './server/utils/logger/copier' )
+const createLogHistory = require('./server/utils/logger/copier');
 const PORT = process.env.PORT || 9632;
 const app = express();
 const http = require('http');
@@ -22,8 +22,8 @@ const server=http.createServer(app);
 server.listen(3000);
 controller.createConnection(server);
 
-setInterval( () => {
-    createLogHistory( CONSTANTS.LOG_FILE_PATH,
-      `${CONSTANTS.DUMPS_PATH}${Date.parse( new Date() )}.json` )
-  },
-  CONSTANTS.DAY_LENGTH )
+setInterval(() => {
+  createLogHistory(CONSTANTS.LOG_FILE_PATH,
+    `${CONSTANTS.DUMPS_PATH}${Date.parse(new Date())}.json`);
+},
+CONSTANTS.DAY_LENGTH);
