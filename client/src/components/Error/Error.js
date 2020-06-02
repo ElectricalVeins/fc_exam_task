0540fff19755cf2399033fc13b93b14e1e4533e8
eveins <maxmost512@gmail.com>
2020-06-02 12:19:18 +0300

    refactor Error component

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -2,28 +2,10 @@ import React from 'react';
 import styles from './Error.module.sass';
 
 const Error=props=>{
-    const getMessage=()=>{
-        const {status,data}=props;
-        switch (status) {
-            case 404:
-                return data;
-            case 400:
-                return 'Check the input data';
-            case 409:
-                return data;
-            case 403:
-                return 'Bank decline transaction';
-            case 406:
-                return data;
-            default:
-                return 'Server Error';
-        }
-    };
-
-    const {clearError}=props;
+    const {clearError,status,data}=props;
     return(
         <div className={styles.errorContainer}>
-            <span>{getMessage()}</span>
+            <span>{data}</span>
             <i className="far fa-times-circle" onClick={()=>clearError()}/>
         </div>
     )
