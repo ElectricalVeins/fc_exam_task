8bdbd3c1c5c3527614c4338f999a45ccf8b1c309
eveins <maxmost512@gmail.com>
2020-06-04 14:48:53 +0300

    server minor refactor
    create boot folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,13 @@
-const http = require('http');
-const express=require('express');
-const cors=require('cors');
-require('./server/dbMongo/mongoose');
 require('babel-polyfill');
-const router=require('./server/router');
+require('./server/dbMongo/mongoose');
 const controller = require('./socketInit');
-const errorHandler=require('./server/middlewares/errorHandlers');
+const server = require('./boot/startHttpServer');
 const CONSTANTS =require('./constants');
 const createLogHistory = require('./server/utils/logger/copier');
-
 const PORT = process.env.PORT || 3000;
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use('/public', express.static('public'));
-app.use(router);
-app.use(errorHandler);
-
-const server=http.createServer(app);
-server.listen(PORT);
 controller.createConnection(server);
+server.listen(PORT);
 
 setInterval(() => {
   createLogHistory(CONSTANTS.LOG_FILE_PATH,
