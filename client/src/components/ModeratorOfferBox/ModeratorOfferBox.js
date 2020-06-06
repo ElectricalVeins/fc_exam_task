d9e1900b4ccc47775ecb107308c9db9b92d43a4a
eveins <maxmost512@gmail.com>
2020-06-06 14:34:03 +0300

    change ModeratorOfferBox.js view

--- client/src/components/ModeratorOfferBox/ModeratorOfferBox.js
+++ client/src/components/ModeratorOfferBox/ModeratorOfferBox.js
@@ -7,14 +7,14 @@ import * as actionCreator from "../../actions/actionCreator";
 import { createSetOfferAction } from "../../actions/actionCreator";
 
 const ModeratorOfferBox = props => {
-  const {id, userId, status, text, fileName, User: {displayName, email}} = props;
+  const {id, text, fileName, User: {displayName, email}} = props;
 
   const banHandler = () => {
-    props.setOffer({id, command: CONSTANTS.OFFER_COMMAND_BAN})
+    props.setOffer({id, command: CONSTANTS.OFFER_COMMAND_BAN, email})
   }
 
   const approveHandler = () => {
-    props.setOffer({id, command: CONSTANTS.OFFER_COMMAND_APPROVE})
+    props.setOffer({id, command: CONSTANTS.OFFER_COMMAND_APPROVE, email})
   }
 
   const onImageHandler = () => {
@@ -37,10 +37,8 @@ const ModeratorOfferBox = props => {
     <section className={styles.moderatorOfferBox}>
       <div className={styles.infoContainer}>
         <h5>Offer ID:<span>{id}</span></h5>
-        <h5>User ID:<span>{userId}</span></h5>
         <h5>User email:<span>{email}</span></h5>
-        <h5>User name:<span>{displayName}</span></h5>
-        <h5>Offer status:<span>{status}</span></h5>
+        <h5>User login:<span>{displayName}</span></h5>
       </div>
       {renderBody()}
       <div className={styles.btnCnt}>
