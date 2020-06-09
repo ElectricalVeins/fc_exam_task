e77353223685968750d95df4c4788bb50b06b5dd
eveins <maxmost512@gmail.com>
2020-06-09 12:35:18 +0300

    add createWhereForOffers in createPredicate utilities functions

--- server/src/server/utils/createPredicate.js
+++ server/src/server/utils/createPredicate.js
@@ -1,4 +1,4 @@
-const bd = require('../models/index');
+const db = require('../models/index');
 const CONSTANTS =require('../../constants');
 
 module.exports.createWhereForAllContests = (types, contestId, industry, awardSort, status) => {
@@ -14,11 +14,26 @@ module.exports.createWhereForAllContests = (types, contestId, industry, awardSor
     Object.assign(predicate.where, { industry });
   if(awardSort)
     predicate.order.push(['prize', awardSort]);
-  Object.assign(predicate.where, { status: status || { [bd.Sequelize.Op.or] : [CONSTANTS.CONTEST_STATUS_FINISHED, CONSTANTS.CONTEST_STATUS_ACTIVE] } });
+  Object.assign(predicate.where, { status: status || { [db.Sequelize.Op.or] : [CONSTANTS.CONTEST_STATUS_FINISHED, CONSTANTS.CONTEST_STATUS_ACTIVE] } });
   predicate.order.push(['id', 'desc']);
   return predicate;
 };
 
 function getPredicateTypes(types){
-  return { [bd.Sequelize.Op.or] : types };
+  return { [db.Sequelize.Op.or] : types };
 }
+
+module.exports.createWhereForOffers = (role,req,ownEntries,userId) => {
+  const predicate={};
+
+  if(role === CONSTANTS.CREATOR){
+    predicate.userId = req.tokenData.userId;
+  }else{
+    predicate.status = {[db.Sequelize.Op.ne]: CONSTANTS.OFFER_STATUS_MODERATING};
+  }
+  if(ownEntries){
+    predicate.userId = userId
+  }
+
+  return predicate;
+};
