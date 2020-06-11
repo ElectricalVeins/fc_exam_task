c1ca94efa140b17d9c004a25728283e74ba5a44e
eveins <maxmost512@gmail.com>
2020-06-11 19:53:56 +0300

    create chatQueries.js

---
+++ server/src/server/controllers/queries/chatQueries.js
@@ -0,0 +1,30 @@
+const Conversation = require('../../models/mongoModels/conversation');
+const Message = require('../../models/mongoModels/Message');
+const Catalog = require('../../models/mongoModels/Catalog');
+const db = require('../../models/index');
+const badRequestError = require('../../errors/BadRequestError');
+
+module.exports.createConversation = async (conditions, update, options) => {
+  const newConversation = await Conversation.findOneAndUpdate(conditions, update, options);
+  if (!newConversation) {
+    throw new badRequestError(new Error('Can not create conversation'));
+  }
+  return newConversation;
+};
+
+module.exports.getConversation = async (conditions, update, options) => {
+  const chat = await Conversation.findOneAndUpdate(conditions, update, options);
+  if (!chat) {
+    throw new badRequestError(new Error('Can not пуе conversation'));
+  }
+  return chat;
+};
+
+module.exports.createMessage = async (message) => {
+  const newMessage = new Message(message);
+  if (!newMessage) {
+    throw new badRequestError(new Error('Can not create message'));
+  }
+  await newMessage.save();
+  return newMessage;
+};
