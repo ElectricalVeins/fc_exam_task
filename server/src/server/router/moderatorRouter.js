e3e2c1facae0239c44c40fac0d9c6f5c4c788cc9
eveins <maxmost512@gmail.com>
2020-06-13 12:12:28 +0300

    refactor moderatorRouter

--- server/src/server/router/moderatorRouter.js
+++ server/src/server/router/moderatorRouter.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const basicMiddlewares = require('../middlewares/basicMiddlewares');
 const userMiddlewares = require('../middlewares/userMiddlewares');
-const tokenMiddlewares = require ('../middlewares/tokenMiddlewares');
+const tokenMiddlewares = require('../middlewares/tokenMiddlewares');
 const offerController = require('../controllers/offerController');
 
 const moderatorRouter = express.Router();
 
-moderatorRouter.get('/moderator/getOffers',
+moderatorRouter.all(
   tokenMiddlewares.verifyToken,
-  basicMiddlewares.canModerateOffers,
+  basicMiddlewares.canModerateOffers
+);
+
+moderatorRouter.get('/moderator/getOffers',
   offerController.getAllUnModeratedOffers
 );
 
 moderatorRouter.post('/moderator/setOffer',
-  tokenMiddlewares.verifyToken,
-  basicMiddlewares.canModerateOffers,
   userMiddlewares.findUserIdByContestId,
   offerController.offerModeration
 );
