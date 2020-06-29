07c8c842ec00850495e5402781d0cc0f8bb80259
eveins <maxmost512@gmail.com>
2020-06-29 11:45:10 +0300

    create checkPermissionToEditCatalog MW

--- server/src/server/middlewares/chatMiddlewares.js
+++ server/src/server/middlewares/chatMiddlewares.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const db = require('../models');
+const RightsError = require('../errors/RightsError');
 
 module.exports.getUserConversationIds = async (req, res, next) => {
   req.conversationList = [];
@@ -13,6 +14,26 @@ module.exports.getUserConversationIds = async (req, res, next) => {
       req.conversationList.push(conversation.ConversationId);
     });
     next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports.checkPermissionToEditCatalog = async (req, res, next) => {
+  try {
+    const { tokenData: { userId }, body: { catalogId } } = req;
+    const catalogs = db.Catalogs.findAll({
+      where: {
+        userId,
+        id: catalogId,
+      },
+    });
+    console.log(catalogs);
+    if (catalogs && catalogs.length !== 1) {
+      next();
+    }
+    next(new RightsError('You dont have rights to edit this catalog'));
   } catch (err) {
     console.log(err);
     next(err);
