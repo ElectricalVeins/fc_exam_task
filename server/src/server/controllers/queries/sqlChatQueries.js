8c480f48f47a84b99c0e49d0db40f5d3408c05ee
eveins <maxmost512@gmail.com>
2020-07-04 13:18:49 +0300

    add getUserConversation query

--- server/src/server/controllers/queries/sqlChatQueries.js
+++ server/src/server/controllers/queries/sqlChatQueries.js
@@ -1,6 +1,14 @@
 const db = require('../../models');
 const badRequestError = require('../../errors/BadRequestError');
 
+module.exports.getUserConversation = async (userPredicate) => {
+  return await db.Conversations.findOne({
+    where: userPredicate,
+    raw: true,
+    nest: true,
+  });
+};
+
 module.exports.getCatalogById = async (id) => {
   return await db.Catalogs.findOne({
     where: {
