aac7c7ed5ff0a344480631b7e9950730487f6f5a
Vladimir <maxmost512@gmail.com>
2020-07-29 23:31:23 +0300

    create timer controller

---
+++ server/src/server/controllers/timerController.js
@@ -0,0 +1,54 @@
+const ServerError = require('../errors/ServerError');
+const timerQueries = require('./queries/timerQueries');
+const CONSTANTS = require('../../constants');
+
+module.exports.getUserTimers = async (req, res, next) => {
+    try {
+        const { tokenData: { userId } } = req;
+        const timers = await timerQueries.findAllUserTimers(userId);
+        res.send(timers);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+};
+
+module.exports.createTimer = async (req, res, next) => {
+    try {
+        const { tokenData: { userId }, body: { name, finalDate, warnDate } } = req;
+        const newTimer = {
+            userId,
+            name,
+            finalDate,
+            warnDate
+        };
+
+        const timer = await timerQueries.createTimer(newTimer);
+        res.send(timer);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+};
+
+module.exports.deleteTimer = async (req, res, next) => {
+    try {
+        const { tokenData: { userId }, query: { id } } = req;
+        const result = await timerQueries.softDeleteTimer(id, userId);
+        res.send({result});
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+};
+
+module.exports.updateTimer = async () => {
+    try {
+        const { tokenData: { userId }, body: { id, name, warnDate, finalDate } } = req;
+        const updatedTimer = await timerQueries.updateTimer({ id, name, warnDate, finalDate }, userId)
+        res.send(updatedTimer);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+};
